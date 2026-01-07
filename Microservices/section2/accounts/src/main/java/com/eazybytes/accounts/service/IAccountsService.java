c82bf0016c9e55e58b3f59d8596e1aef40d3a0f3
marcellius-533 <marcelliusfelixmatius@gmail.com>
2026-01-07 16:55:13 +0700

    07/01/2025 menambahkan read api

--- Microservices/section2/accounts/src/main/java/com/eazybytes/accounts/service/IAccountsService.java
+++ Microservices/section2/accounts/src/main/java/com/eazybytes/accounts/service/IAccountsService.java
@@ -10,4 +10,11 @@ public interface IAccountsService {
      */
     void createAccount(CustomerDto customerDto);
 
+    /**
+     * 
+     * @param mobileNumber
+     * @return Accounts details based on a given mobileNumber
+     */
+    CustomerDto fetchAccount(String mobileNumber);
+
 }
