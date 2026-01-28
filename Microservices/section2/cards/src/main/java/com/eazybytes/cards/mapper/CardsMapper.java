bb3a0ca75e5a70f52aaf02a5f1a420f25f017e34
marcellius-533 <marcelliusfelixmatius@gmail.com>
2026-01-28 23:04:42 +0700

    28/01/2026 melengkapi kebutuhan cards entitas

---
+++ Microservices/section2/cards/src/main/java/com/eazybytes/cards/mapper/CardsMapper.java
@@ -0,0 +1,30 @@
+package com.eazybytes.cards.mapper;
+
+
+import org.springframework.stereotype.Component;
+
+import com.eazybytes.cards.dto.CardsDTO;
+import com.eazybytes.cards.entity.Cards;
+
+@Component
+public class CardsMapper {
+    public CardsDTO mapToCardsDto(Cards cards, CardsDTO cardsDTO){
+        cardsDTO.setCardNumber(cards.getCardNumber());
+        cardsDTO.setCardType(cards.getCardType());
+        cardsDTO.setMobileNumber(cards.getMobileNumber());
+        cardsDTO.setTotalLimit(cards.getTotalLimit());
+        cardsDTO.setAvailableAmount(cards.getAvailableAmount());
+        cardsDTO.setAmountUsed(cards.getAmountUsed());
+        return cardsDTO;
+    }
+
+    public Cards mapToCards(CardsDTO cardsDTO, Cards cards){
+        cards.setCardNumber(cardsDTO.getCardNumber());
+        cards.setCardType(cardsDTO.getCardType());
+        cards.setMobileNumber(cardsDTO.getMobileNumber());
+        cards.setTotalLimit(cardsDTO.getTotalLimit());
+        cards.setAvailableAmount(cardsDTO.getAvailableAmount());
+        cards.setAmountUsed(cardsDTO.getAmountUsed());
+        return cards;
+    }
+}
