29513eaf1dc9b50d0759cbb5797f2259abeed65b
marcellius-533 <marcelliusfelixmatius@gmail.com>
2026-04-19 17:10:28 +0700

    2026/04/19 belajar sampai buildpacks

--- Microservices/section4/loans/src/main/java/com/example/loans/entity/Loans.java
+++ Microservices/section4/loans/src/main/java/com/example/loans/entity/Loans.java
@@ -12,7 +12,7 @@ import lombok.ToString;
 
 @Entity
 @Getter @Setter @ToString @AllArgsConstructor @NoArgsConstructor
-public class Loans {
+public class Loans extends BaseEntity {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
